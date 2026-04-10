cf58427d8bf7fb7f1e9a720a174ddc4f1605241b
KedarPanchal <kedar.t.panchal@outlook.com>
2026-04-10 16:52:46 -0500

    Support 3 different backtesters

--- backtesting/backtest.sh
+++ backtesting/backtest.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# Ensure all backtesters are installed and up to date
 uv sync
-uv run prosperity4btx "$@"
+
+# The first argument is which backtester to use
+backtester="$1"
+
+if [[ "$backtester" == "zeeshan" ]]; then
+    uv run prosperity4btx "$@"
+elif [[ "$backtester" == "nabayansaha" ]]; then
+    uv run prosperity4btest "$@"
+elif [[ "$backtester" == "jmerle" ]]; then
+    uv run prosperity3bt "$@"
+else
+    echo "Unknown backtester: $backtester"
+fi
